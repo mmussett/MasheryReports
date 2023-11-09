4f3d07b2514f9f5f7d9a8bf150f10d200ca7eb1d
mmussett <mmussett@me.com>
2023-11-09 17:33:27 +0000

    Added getAllMemberPackages extract script

--- getAllMembersPackageKeys.sh
+++ getAllMembersPackageKeys.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Get security details for endpoints.
-# Output in CSV.
+# Extracts all Members with associated Applications and PackageKeys
+# Output in JSON
 
-# (c) 2022 TIBCO Software Inc.
+# (c) 2023 TIBCO Software Inc.
 #The following information is confidential information of TIBCO Software Inc. Use, duplication, transmission, or republication for any purpose #without the prior written consent of TIBCO is expressly prohibited.
 
 #This document (including, without limitation, any product roadmap or statement of direction data) illustrates the planned testing, release and availability dates for TIBCO products and services. This document is provided for informational purposes only and its contents are subject to change without notice. TIBCO makes no warranties, express or implied, in or relating to this document or any information in it, including, without limitation, that this document, or any information in it, is error-free or meets any conditions of merchantability or fitness for a particular purpose. This document may not be reproduced or transmitted in any form or by any means without our prior written permission.
 #The material provided is for informational purposes only, and should not be relied on in making a purchasing decision. The information is not a commitment, promise or legal obligation to deliver any material, code, or functionality. The development, release, and timing of any features or functionality described for our products remain at our sole discretion.
 #In this document TIBCO or its representatives may make forward-looking statements regarding future events, TIBCO's future results or our future financial performance. These statements are based on management's current expectations. Although we believe that the expectations reflected in the forward-looking statements contained in this document are reasonable, these expectations or any of the forward-looking statements could prove to be incorrect and actual results or financial performance could differ materially from those stated herein. TIBCO does not undertake to update any forward-looking statement that may be made from time to time or on its behalf.
 
-# 20221114 - MMussett - First version (for GATX).
+# 20231109 - MMussett - First version.
 
 
 #
@@ -35,10 +35,9 @@ LIMIT=1000
 
 # Output usage information
 function usage() {
-	echo "usage: $NAME -u username -p password -k apiKey -s apiSecret -a areaUuid [-d apiDefinition]"
-	echo " Get a list of all API Endpoints and their use of SSL."
-	echo "    $NAME -u dev@tibco.com -p mypwd123 -k 5cwt42e494bb4h7fkxsh6xh6 -s Sq4UjgLZ2U -a 12b2de7a-c456-4cc0-893d-4bbf3123be2b > 12b2de7a-c456-4cc0-893d-4bbf3123be2b.tsv"
-	echo "    $NAME -u dev@tibco.com -p mypwd123 -k 5cwt42e494bb4h7fkxsh6xh6 -s Sq4UjgLZ2U -a 12b2de7a-c456-4cc0-893d-4bbf3123be2b -d 2mf7trqbz84nr8j6kx848er5 > 2mf7trqbz84nr8j6kx848er5.tsv"
+	echo "usage: $NAME -u username -p password -k apiKey -s apiSecret -a areaUuid"
+	echo " Extract all members with their applications and package keys."
+	echo "    $NAME -u dev@tibco.com -p mypwd123 -k 5cwt42e494bb4h7fkxsh6xh6 -s Sq4UjgLZ2U -a 12b2de7a-c456-4cc0-893d-4bbf3123be2b > extract.json"
 	exit 1
 }
 
